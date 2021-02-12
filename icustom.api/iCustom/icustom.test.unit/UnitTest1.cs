4a81e60a26508b8450a95d5f01514edf30f853d0
Wallace da Cunha Augusto <wallace_cunha_augusto@hotmail.com>
2021-02-12 17:39:49 -0300

    Versão Inicial do Projeto

---
+++ icustom.api/iCustom/icustom.test.unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace icustom.test.unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
